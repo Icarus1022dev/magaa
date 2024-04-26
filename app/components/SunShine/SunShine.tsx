ff652580673d894d2e5b59daf206775aa0b486f9
cryptoDev <icarus.senior.dev@gmail.com>
2024-04-26 10:47:48 -0700

    fixed animation

---
+++ app/components/SunShine/SunShine.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './style.css'
+
+const SunShine = () => {
+  return (
+    <div className='sunCon'>
+      <div className="sun">
+        <div className="ray_box">
+          <div className="ray ray1"></div>
+          <div className="ray ray2"></div>
+          <div className="ray ray3"></div>
+          <div className="ray ray4"></div>
+          <div className="ray ray5"></div>
+          <div className="ray ray6"></div>
+          <div className="ray ray7"></div>
+          <div className="ray ray8"></div>
+          <div className="ray ray9"></div>
+          <div className="ray ray10"></div>
+          <div className="ray ray11"></div>
+          <div className="ray ray12"></div>
+          <div className="ray ray13"></div>
+          <div className="ray ray14"></div>
+          <div className="ray ray15"></div>
+          <div className="ray ray16"></div>
+          <div className="ray ray17"></div>
+          <div className="ray ray18"></div>
+          <div className="ray ray19"></div>
+          <div className="ray ray20"></div>
+          <div className="ray ray21"></div>
+        </div>
+      </div>
+      <div className='imgContainer'>
+        <img className='rainbowImg' src='./assets/images/rainbow 10.png' />
+      </div>
+    </div >
+  )
+}
+
+export default SunShine
